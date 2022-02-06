572b2cd867d595b6fbdeda7025a8623db78c76c5
anallely753 <anallely.mtzc@comunidad.unam.mx>
2022-02-05 23:34:37 -0600

    Models, views,templates

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    quantity = models.FloatField(default=0)
+    unit = models.CharField(max_length=50)
+    unit_price = models.FloatField(default=0)
+
+class MenuItem(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    price = models.FloatField(default=0.00)
+    
+class RecipeRequirement(models.Model):
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    quantity = models.FloatField(default=0)
+    
+class Purchase(models.Model):
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import Ingredient
+
+from django.views.generic import ListView
 
 # Create your views here.
+
+
+def home(request):
+   return render(request, 'inventory/home.html')
+
+class IngredientList(ListView):
+   model = Ingredient
+   template_name="inventory/ingredient.html"
